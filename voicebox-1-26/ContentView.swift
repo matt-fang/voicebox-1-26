69292f18140b37244ad71562b698f5f2a1dd6ce3
Matthew Fang <matthewfang.calif@gmail.com>
2026-01-26 20:26:36 -0800

    finished compass service and test view

--- voicebox-1-26/ContentView.swift
+++ voicebox-1-26/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var compassService: CompassService = CompassService()
+    
+    @State private var counter = 0
+    
     var body: some View {
         VStack {
+            Text(String(compassService.heading))
+            Button {
+                compassService.isStarted ? compassService.stop() : compassService.start()
+            } label: {
+                Text("start compass!~")
+            }
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button {
+                print("hello world")
+                counter += 1
+            } label: {
+                Text("press for haptics!")
+            }
+            .sensoryFeedback(.impact(weight: .heavy, intensity: 1), trigger: counter)
         }
         .padding()
     }
---
+++ voicebox-1-26/Service/CompassService.swift
@@ -0,0 +1,38 @@
+//
+//  CompassService.swift
+//  voicebox-1-26
+//
+//  Created by Matthew Fang on 1/26/26.
+//
+
+import Foundation
+import CoreLocation
+import Observation
+
+@Observable
+class CompassService: NSObject, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager = CLLocationManager()
+    var heading: Double = 0.0
+    var isStarted: Bool = false
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization( )
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        heading = newHeading.magneticHeading
+    }
+    
+    func start() {
+        locationManager.startUpdatingHeading()
+        isStarted = true
+    }
+    
+    func stop() {
+        locationManager.stopUpdatingHeading()
+        isStarted = false
+    }
+}
