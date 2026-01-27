dbbc84475d8d67ab6ed27954fdcb4f4ab8d8fd3c
Matthew Fang <matthewfang.calif@gmail.com>
2026-01-26 19:30:52 -0800

    Initial Commit

---
+++ voicebox-1-26/voicebox_1_26App.swift
@@ -0,0 +1,17 @@
+//
+//  voicebox_1_26App.swift
+//  voicebox-1-26
+//
+//  Created by Matthew Fang on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct voicebox_1_26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ voicebox-1-26Tests/voicebox_1_26Tests.swift
@@ -0,0 +1,17 @@
+//
+//  voicebox_1_26Tests.swift
+//  voicebox-1-26Tests
+//
+//  Created by Matthew Fang on 1/26/26.
+//
+
+import Testing
+@testable import voicebox_1_26
+
+struct voicebox_1_26Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
